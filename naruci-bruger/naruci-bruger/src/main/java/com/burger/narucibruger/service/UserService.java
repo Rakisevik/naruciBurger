1117a4b51678952e58df1183b85d3043b6df4006
Rakisevik <stefan.rakicevic.4228@metropolitan.ac.rs>
2021-01-10 23:26:42 +0100

    sixth update

--- naruci-bruger/naruci-bruger/src/main/java/com/burger/narucibruger/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.burger.narucibruger.service;
-
-import com.burger.narucibruger.dto.UserRegistrationDto;
-import com.burger.narucibruger.model.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    User save(UserRegistrationDto registrationDto);
-}
